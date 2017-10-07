42d5e0e7ff4875b1fb60b5adda14fdd584e1b7b2
vochicong <kongvc@gmail.com>
2017-10-08 00:43:57 +0900

    Install Ruby 2.4.2 for Mac

--- mac/scripts/24-install-ruby.sh
+++ mac/scripts/24-install-ruby.sh
@@ -1,12 +1,12 @@
 set -ex
 
-source ~/.bashrc
-brew link readline --force
-export CFLAGS=-Wno-error=shorten-64-to-32
-CONFIGURE_OPTS="--with-readline-dir=/usr/local --with-openssl-dir=`brew --prefix openssl`" \
-  RUBY_CONFIGURE_OPTS="--with-openssl-dir=`brew --prefix openssl`" \
-  rbenv install 2.3.3 --skip-existing
-rbenv global 2.3.3
+brew install rbenv ruby-build
+
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+# Install Ruby
+rbenv install 2.4.2
+rbenv global 2.4.2
 ruby -v
 
-gem install rubocop
+gem install bundler
