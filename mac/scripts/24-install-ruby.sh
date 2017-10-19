4c21134c023ecb34fbc6ef1991c77958b74f3328
Cong <congvc@cloudsource.co.jp>
2017-10-20 07:06:40 +0900

    Mac: add rbenv-gemset, rubocop, rspec

--- mac/scripts/24-install-ruby.sh
+++ mac/scripts/24-install-ruby.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 set -ex
 
-brew install rbenv ruby-build
+brew install rbenv ruby-build rbenv-gemset
 
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
@@ -9,4 +10,4 @@ rbenv install 2.4.2
 rbenv global 2.4.2
 ruby -v
 
-gem install bundler
+gem install bundler rubocop-git rspec
